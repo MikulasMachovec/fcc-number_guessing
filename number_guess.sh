09e1429349bb99b704cd7994e13ea19336fb33e1
Mikulas Machovec <mikimachovec@gmail.com>
2025-01-09 13:38:36 +0000

    feat: basic game functionality done

--- number_guess.sh
+++ number_guess.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=num_guessing -t --no-align -c"
 
-USER_LOGIN(){
-
 echo "Enter your username:"
 read NAME
 # find user_id
@@ -24,14 +22,44 @@ if [[ -z $USER_ID ]]
    BEST=$($PSQL "SELECT best_game FROM users WHERE user_id=$USER_ID")
   echo "Welcome back, $USERNAME! You have played $PLAYED games, and your best game took $BEST guesses."
 fi
-}
+
+#After game player update fun
+
+
+
 
 GAME(){
-echo $((1 + RANDOM % 1000))
+RANDOM_NUMBER=$((1 + RANDOM % 1000))
+GUESSED=false
+echo $RANDOM_NUMBER
+TRIES=0
+echo "Guess the secret number between 1 and 1000:"
+while [[ $GUESSED == false  ]]
+do 
+  read GUESS
+  if [[ $GUESS =~ ^[0-9+$] ]]
+  then
+    ((TRIES++))
+    if [[ $GUESS == $RANDOM_NUMBER ]]
+    then
+      echo "You guessed it in $TRIES tries. The secret number was $RANDOM_NUMBER. Nice job!"
+      
+      GUESSED=true
+    elif [[ $GUESS -lt $RANDOM_NUMBER  ]]
+    then
+      echo "It's higher than that, guess again:"
+    else
+      echo "It's lower than that, guess again:"
+    fi
+    
+  else
+  echo "That is not an integer, guess again:"
+  fi
+
+done
 }
 
 MAIN_GAME(){
-  USER_LOGIN
   GAME
 }
 
